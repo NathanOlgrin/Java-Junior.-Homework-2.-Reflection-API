4ac63b443c664133dcbce4822bab444a34d882bf
NathanOlgrin <matrica_1997@mail.ru>
2024-07-11 12:57:48 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+=======
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Arrays.stream(Class.forName("java.lang.String").getMethods()).forEach(System.out::println);
+>>>>>>> 79d0121 (First commit)
     }
 }
